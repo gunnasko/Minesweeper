78be432ce9fc0573399339fbab07b37f44a64448
Gunnar Andreas Skogvold <andreas.skogvold@conceptos.no>
2019-01-30 17:04:12 +0100

    Created class of board and points

---
+++ Minesweeper/Board.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    public class Board
+    {
+        private List<Point> points;
+        private int numOfColumns;
+        private int numOfRows;
+        private int numOfMines;
+
+        public Board(int boardColumnSize, int boardRowSize, int numOfMinesInGames)
+        {
+            numOfColumns = boardColumnSize;
+            numOfRows = boardRowSize;
+            numOfMines = numOfMinesInGames;
+        }
+
+        public Board() : this(8, 8, 10)
+        {
+        }
+
+        public void FillPoints()
+        {
+
+        }
+    }
+}
---
+++ Minesweeper/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    public struct Coordinate
+    {
+        public int x;
+        public int y;
+    }
+    public class Point
+    {
+        public bool HasMine { get; }
+        public bool IsFlagged { get; set; }
+        Coordinate PointCoordinate { get; }
+        public bool IsOpenend { get; set; }
+        public int NumOfAdjacentMines { get; set; }
+
+        public Point(bool hasMine, Coordinate coordinate)
+        {
+            PointCoordinate = coordinate;
+            HasMine = hasMine;
+        }
+    }
+}
