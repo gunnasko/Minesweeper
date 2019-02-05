2d2152a0c107f2818394db78d9454296cf04a31d
Gunnar Andreas Skogvold <andreas.skogvold@conceptos.no>
2019-02-05 15:24:25 +0100

    Added scoreboard and tests logic

---
+++ Minesweeper/MineSweeper.Tests/HighScore.Test.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Minesweeper.Tests
+{
+    [TestClass]
+    public class HighScoreTests
+    {
+        [TestMethod]
+        public void ShouldBeAbleToAddScoresToAllDifficultiesExceptCustom()
+        {
+            var highscore = new HighScores();
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "BeginnerScore",
+                Score = 5
+            }, GameDifficulty.Beginner);
+
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "IntermediateScore",
+                Score = 10
+            }, GameDifficulty.Intermediate);
+
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "ExpertScore",
+                Score = 15
+            }, GameDifficulty.Expert);
+
+            var beginnerList = highscore.GetTopTen(GameDifficulty.Beginner);
+            var intermediateList = highscore.GetTopTen(GameDifficulty.Intermediate);
+            var expertList = highscore.GetTopTen(GameDifficulty.Expert);
+
+            Assert.AreEqual(5, beginnerList[0].Score);
+            Assert.AreEqual(10, intermediateList[0].Score);
+            Assert.AreEqual(15, expertList[0].Score);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldNotBeAbleToAddCustomDifficultyHighscore()
+        {
+            var highscore = new HighScores();
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "CustomScore",
+                Score = 5
+            }, GameDifficulty.Custom);
+        }
+
+        [TestMethod]
+        public void HighscoreShouldOnlyReturnTopTenInDescendingValue()
+        {
+            var highscore = new HighScores();
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "Andy",
+                Score = 10
+            }, GameDifficulty.Beginner);
+
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "Sam",
+                Score = 5
+            }, GameDifficulty.Beginner);
+
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "Nina",
+                Score = 8
+            }, GameDifficulty.Beginner);
+
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "Jack",
+                Score = 2
+            }, GameDifficulty.Beginner);
+
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "Sammy",
+                Score = 110
+            }, GameDifficulty.Beginner);
+
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "Ingrid",
+                Score = 0
+            }, GameDifficulty.Beginner);
+
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "Sigrid",
+                Score = 44
+            }, GameDifficulty.Beginner);
+
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "Jens",
+                Score = 22
+            }, GameDifficulty.Beginner);
+
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "Elizabeth",
+                Score = 45
+            }, GameDifficulty.Beginner);
+
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "Frank",
+                Score = 63
+            }, GameDifficulty.Beginner);
+
+            highscore.AddHighScore(new HighScoreEntry
+            {
+                Name = "Bob",
+                Score = 6
+            }, GameDifficulty.Beginner);
+
+
+
+            var highScoreSorted = highscore.GetTopTen(GameDifficulty.Beginner);
+
+            Assert.AreEqual(10, highScoreSorted.Count);
+            Assert.AreEqual("Ingrid", highScoreSorted[0].Name);
+            Assert.AreEqual("Jack", highScoreSorted[1].Name);
+            Assert.AreEqual("Sam", highScoreSorted[2].Name);
+            //Frank is last on the list, since Sammy is so low she should not appear on it.
+            Assert.AreEqual("Frank", highScoreSorted[9].Name);
+        }
+
+
+    }
+}
---
+++ Minesweeper/MineSweeper/HighScore.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    public struct HighScoreEntry
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    [DataContract]
+    public class HighScores
+    {
+        [DataMember]
+        private Dictionary<GameDifficulty, List<HighScoreEntry>> _highScores;
+
+        public HighScores()
+        {
+            _highScores = new Dictionary<GameDifficulty, List<HighScoreEntry>>();
+
+            _highScores[GameDifficulty.Beginner] = new List<HighScoreEntry>();
+            _highScores[GameDifficulty.Intermediate] = new List<HighScoreEntry>();
+            _highScores[GameDifficulty.Expert] = new List<HighScoreEntry>();
+        }
+
+        public void AddHighScore(HighScoreEntry entry, GameDifficulty scoreDifficulty)
+        {
+            try
+            {
+                _highScores[scoreDifficulty].Add(entry);
+            }
+            catch (KeyNotFoundException)
+            {
+                throw new ArgumentException("Game difficulty cannot be custom!");
+            }
+        }
+
+        public List<HighScoreEntry> GetTopTen(GameDifficulty highScoreDifficulty)
+        {
+            var query = from h in _highScores[highScoreDifficulty] orderby h.Score select h;
+            return query.Take(10).ToList();
+        }
+
+    }
+}
